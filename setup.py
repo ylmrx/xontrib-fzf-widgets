8af47d1d684a14eb776485ef6f5c30c8e6807f60
David Strobach <lalochcz@gmail.com>
2020-10-16 12:41:39 +0200

    Tag a release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xontrib-fzf-widgets',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/laloch/xontrib-fzf-widgets',
     license='GPLv3',
     author='David Strobach',
